c3c9e85c58b82120974c0bfe3eae250cb4d37134
Giuseppe Matheus Pereira <giuseppemath@gmail.com>
2022-11-01 14:46:43 -0300

    (refactoring) migrating everything to lua
    Projectionist activations

---
+++ lua/user/plugins/configs/projectionist.lua
@@ -0,0 +1,51 @@
+local g = vim.g
+
+g.projectionist_heuristics = {
+    -- activation in ruby
+    [ "etc/rbenv.d/|bin/rbenv-*" ] = {
+        -- projections for ruby
+        [ "bin/rbenv-*" ] = {
+            type= "command",
+            template = { "#!/usr/bin/env bash" },
+        },
+        [ "etc/rbenv.d/*.bash" ] = {type= "hook"}
+    },
+    -- activation in java projects
+    [ 'pom.xml|build.gradle|settings.gradle'] = {
+
+        -- projections for java
+        [ 'src/main/java/*.java' ] = {
+            type= "source",
+            alternate= "src/test/java/{}Test.java"
+        },
+        [ 'src/test/java/*Test.java' ] = {
+            type= "source",
+            alternate= "src/main/java/{}.java"
+        }
+    },
+    -- activation in javascript/typescript
+    ['settings.json|.nodejs|.angular|.react'] = {
+
+        -- projections for tsx
+        ['src/*.tsx'] = {
+            alternate = "src/{}.test.tsx"
+        },
+        ['src/*.test.tsx'] = {
+            alternate = "src/{}.tsx"
+        },
+        -- projections for js
+        ['src/*.js'] = {
+            alternate = "src/{}.test.js"
+        },
+        ['src/*.test.js'] = {
+            alternate = "src/{}.js"
+        },
+        -- projections for jsx
+        ['src/*.jsx'] = {
+            alternate = "src/{}.test.jsx"
+        },
+        ['src/*.test.jsx'] = {
+            alternate = "src/{}.jsx"
+        }
+    }
+}
