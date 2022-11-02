6afcd7f9394015e869e9b80866f731946c9de562
Giuseppe Matheus (Beppe) <giuseppemath@gmail.com>
2022-11-01 23:40:46 -0300

    (refactoring) migrating everything to lua
    add write command before testing buffer, useful for TDD workflows

--- lua/user/tests/keymaps.lua
+++ lua/user/tests/keymaps.lua
@@ -1,12 +1,17 @@
 local u = require'user.keymaps._utils'
 local n = u.n
+local neotest = require 'neotest'
 
+
+local function write_buffer()
+    vim.cmd("write")
+end
 
 -- default testing keymaps
 n('<Leader>tt', ':TestFile<CR>')
-n('<Leader>t', function() require('neotest').run.run() end)
-n('<Leader>tf', function() require('neotest').run.run(vim.fn.expand("%")) end)
-n('<Leader>td', function() require('neotest').run.run({strategy = "dap"}) end)
+n('<Leader>t' , function() write_buffer() neotest.run.run() end)
+n('<Leader>tf', function() write_buffer() neotest.run.run(vim.fn.expand("%")) end)
+n('<Leader>td', function() write_buffer() neotest.run.run({strategy = "dap"}) end)
 
 
 -- change keymaps based on buffer extesion
