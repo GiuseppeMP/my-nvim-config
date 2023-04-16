4f7242e439787b9f4ea26fba6ca0af3e997c9f93
Peppe <giuseppemath@gmail.com>
2023-04-16 15:56:01 -0300

    chore(nvterm) make float terminal bigger

--- lua/user/plugins/configs/nvterm.lua
+++ lua/user/plugins/configs/nvterm.lua
@@ -1,16 +1,16 @@
 require("nvterm").setup({
-  terminals = {
-    type_opts = {
-      float = {
-        relative = 'editor',
-        row = 0.3,
-        col = 0.25,
-        width = 0.5,
-        height = 0.4,
-        border = "single",
-      },
-      horizontal = { location = "rightbelow", split_ratio = .3, },
-      vertical = { location = "rightbelow", split_ratio = .5 },
-    },
-  }
+    terminals = {
+        type_opts = {
+            float = {
+                relative = 'editor',
+                row = 0.1,
+                col = 0.1,
+                width = 0.8,
+                height = 0.8,
+                border = "single",
+            },
+            horizontal = { location = "rightbelow", split_ratio = .3, },
+            vertical = { location = "rightbelow", split_ratio = .5 },
+        },
+    }
 })
