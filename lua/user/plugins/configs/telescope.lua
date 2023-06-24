cffc314cda408720314c488276e45a3766aec7ed
Peppe <giuseppemath@gmail.com>
2023-06-24 17:14:54 -0300

    fix(telescope) temp workaround to avoid open files in insert mode

--- lua/user/plugins/configs/telescope.lua
+++ lua/user/plugins/configs/telescope.lua
@@ -39,3 +39,12 @@ require("telescope").setup {
         },
     }
 }
+
+-- fix issue https://github.com/nvim-telescope/telescope.nvim/issues/2027 introduced by neovim >= 0.9.0
+vim.api.nvim_create_autocmd("WinLeave", {
+    callback = function()
+        if vim.bo.ft == "TelescopePrompt" and vim.fn.mode() == "i" then
+            vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "i", false)
+        end
+    end,
+})
