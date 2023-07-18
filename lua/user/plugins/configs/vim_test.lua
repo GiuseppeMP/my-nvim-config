6d2a04ea3d6e11a3dcc480509b089364babb8f96
Peppe <giuseppemath@gmail.com>
2023-07-18 11:50:30 -0300

    feat(coverage): add blanket for jacoco coverage, and nvimcoverage for lcov plugins.

--- lua/user/plugins/configs/vim_test.lua
+++ lua/user/plugins/configs/vim_test.lua
@@ -3,7 +3,7 @@ local g = vim.g
 g.ultest_use_pty = 1
 
 g['test#strategy'] = {
-   nearest = 'asyncrun_background',
-   file =    'asyncrun_background',
-   suite =   'asyncrun_background',
+   nearest = 'neovim',
+   file =    'neovim',
+   suite =   'neovim',
 }
--- lua/user/plugins/install/plugins.lua
+++ lua/user/plugins/install/plugins.lua
@@ -492,6 +492,42 @@ M.plugins = {
         "folke/todo-comments.nvim",
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function() require("todo-comments").setup() end
+    },
+    {
+        "andythigpen/nvim-coverage",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function() require("coverage").setup() end
+    },
+    {
+        "dsych/blanket.nvim",
+        config = function()
+            require("blanket").setup({
+                -- can use env variables and anything that could be interpreted by expand(), see :h expandcmd()
+                -- OPTIONAL
+                report_path = vim.fn.getcwd() .. "/target/site/jacoco/jacoco.xml",
+                -- refresh gutter every time we enter java file
+                -- defauls to empty - no autocmd is created
+                filetypes = "java",
+                -- for debugging purposes to see whether current file is present inside the report
+                -- defaults to false
+                silent = true,
+                -- can set the signs as well
+                signs = {
+                    priority = 10,
+                    incomplete_branch = "┃",
+                    uncovered = "┃",
+                    covered = "┃",
+                    sign_group = "Blanket",
+
+                    -- and the highlights for each sign!
+                    -- useful for themes where below highlights are similar
+                    incomplete_branch_color = "yellow",
+                    covered_color = "green",
+                    uncovered_color = "Error",
+                },
+
+            })
+        end
     }
 }
 return M.plugins
