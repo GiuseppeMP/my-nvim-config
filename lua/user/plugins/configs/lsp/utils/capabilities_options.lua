9e4f6a04d396d6a00daa1d40701c27d57ee8ff9c
Peppe <giuseppemath@gmail.com>
2023-08-19 22:23:26 -0300

    chore(cpm) change update -> default capabilities

--- lua/user/plugins/configs/lsp/utils/capabilities_options.lua
+++ lua/user/plugins/configs/lsp/utils/capabilities_options.lua
@@ -4,7 +4,7 @@ M.get = function(params)
     local client_capabilities = vim.lsp.protocol.make_client_capabilities()
     client_capabilities.textDocument.completion.completionItem.snippetSupport = true
     if (params.cmp_nvim == nil or params.cmp_nvim) then
-        return require('cmp_nvim_lsp').update_capabilities(client_capabilities)
+        return require('cmp_nvim_lsp').default_capabilities(client_capabilities)
     else
         return client_capabilities
     end
