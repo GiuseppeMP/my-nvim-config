36b08142bc212ffcd9a9764438874419fa0eb3f0
Peppe <giuseppemath@gmail.com>
2023-10-20 22:21:36 -0300

    (fix): nui winblend, dressing

--- lua/user/plugins/configs/dressing.lua
+++ lua/user/plugins/configs/dressing.lua
@@ -1,15 +1,8 @@
 require('dressing').setup({
-    -- select = {
-    --   get_config = function(opts)
-    --     if opts.kind == 'codeaction' then
-    --       return {
-    --         backend = 'nui',
-    --         nui = {
-    --           relative = 'cursor',
-    --           max_width = 40,
-    --         }
-    --       }
-    --     end
-    --   end
-    -- }
+    input = {
+        relative = "cursor",
+        win_options = {
+            winblend = 0,
+        },
+    },
 })
