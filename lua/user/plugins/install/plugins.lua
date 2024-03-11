9505b6f61f198e9f8914efd4bdbbf61e2b2326ae
GiuseppeMP <giuseppemath@gmail.com>
2024-03-11 19:59:31 -0300

    (feat): pastify, kmonad syntax

--- lua/user/plugins/install/plugins.lua
+++ lua/user/plugins/install/plugins.lua
@@ -176,7 +176,12 @@ M.plugins = {
     -- tmux <-> neovim navigation using C-l,k,j,h
     { 'christoomey/vim-tmux-navigator' },
 
-    { 'iamcco/markdown-preview.nvim',  build = 'cd app && yarn install' },
+    {
+        'iamcco/markdown-preview.nvim',
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
 
     -- Markdown preview
     {
@@ -1075,6 +1080,26 @@ M.plugins = {
     {
         "SmiteshP/nvim-navic"
     },
-
+    {
+        'kmonad/kmonad-vim'
+    },
+    {
+        'TobinPalmer/pastify.nvim',
+        cmd = { 'Pastify' },
+        config = function()
+            require('pastify').setup {
+                opts = {
+                    absolute_path = false,        -- use absolute or relative path to the working directory
+                    apikey = '',                  -- Api key, required for online saving
+                    local_path = '/assets/imgs/', -- The path to put local files in, ex ~/Projects/<name>/assets/images/<imgname>.png
+                    save = 'local',               -- Either 'local' or 'online'
+                },
+                ft = {                            -- Custom snippets for different filetypes, will replace $IMG$ with the image url
+                    html = '<img src="$IMG$" alt="">',
+                    markdown = '![]($IMG$)',
+                    tex = [[\includegraphics[width=\linewidth]{$IMG$}]],
+                }, }
+        end
+    }
 }
 return M.plugins
