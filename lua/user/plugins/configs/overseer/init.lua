f1d3e48c4f538b14cdb83d56590978fc5cfc18b9
Micci <luiz.miccieli@thoughtworks.com>
2024-05-23 16:10:47 -0300

    (feat): added task for robotframework

--- lua/user/plugins/configs/overseer/init.lua
+++ lua/user/plugins/configs/overseer/init.lua
@@ -1,6 +1,7 @@
 local overseer = require("overseer")
 local spring_boot_tasks = require("user.plugins.configs.overseer.spring_boot")
 local mvn_tasks = require("user.plugins.configs.overseer.mvn")
+local robot_tasks = require("user.plugins.configs.overseer.robot_framework")
 
 overseer.setup({
     strategy = {
@@ -31,4 +32,8 @@ end
 
 for _, mvn_task in pairs(mvn_tasks) do
     overseer.register_template(mvn_task)
+end
+
+for _, robot_task in pairs(robot_tasks) do
+    overseer.register_template(robot_task)
 end
