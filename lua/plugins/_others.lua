dfa8c1b54951bcfdeb24ddec1c167f87b5c089f2
Peppe <giuseppemath@gmail.com>
2024-06-29 10:27:00 -0300

    (refactor): migrating gitsigns

--- lua/plugins/_others.lua
+++ lua/plugins/_others.lua
@@ -1,37 +1,18 @@
 return {
     { 'lewis6991/async.nvim' },
-    { 'lewis6991/gitsigns.nvim' },
     { 'mbbill/undotree' },
-    -- Repeat plugins cmds using dot command
-    -- Repetir os cmds dos plugins usando ponto
     { 'tpope/vim-repeat' },
-
-    -- Powerline fonts for VIM
-    -- Fonts powerline para VIM
     { 'powerline/powerline-fonts' },
-
-    -- Fast comments using gcc or v(selection) gcc
-    -- Comentarios rapidos atraves de gcc ou v(selecao)gcc
     { "tpope/vim-commentary" },
-
-
-    -- auto close and auto rename xml, htmls tags
     { 'windwp/nvim-ts-autotag' },
-
-    -- Rainbow in Treesitter
     -- { 'HiPhish/nvim-ts-rainbow2',                    event = 'VeryLazy' },
     { 'https://gitlab.com/HiPhish/rainbow-delimiters.nvim', event = 'VeryLazy' },
-
-    -- close buffers without exit vim or messing layouts
     { 'moll/vim-bbye' },
-
-    -- Plugins for VIM :colorscheme collections
     { 'rafi/awesome-vim-colorschemes' },
     { 'artanikin/vim-synthwave84' },
     { 'ryanoasis/vim-devicons' },
     { 'Mofiqul/dracula.nvim' },
     { 'mfussenegger/nvim-treehopper',                       dependencies = 'smoka7/hop.nvim' },
-    -- new surround plugin use (sa for add surround, sd for delete surround)
     {
         'echasnovski/mini.surround',
         version = '*',
@@ -44,9 +25,7 @@ return {
     },
     { 'tpope/vim-fugitive' },
     { 'nvim-lua/plenary.nvim', },
-    -- vim tests
     { 'kana/vim-vspec', },
-    -- fix cursor in tests/debug
     { 'antoinemadec/FixCursorHold.nvim' },
     { 'voldikss/vim-floaterm' },
     { "SmiteshP/nvim-navic" },
@@ -59,7 +38,6 @@ return {
         event = "InsertEnter",
         opts = {}
     },
-    -- lsp kind icons
     { 'onsails/lspkind.nvim' },
     { 'skywind3000/asyncrun.vim' },
 }
--- lua/plugins/dressing.lua
+++ lua/plugins/dressing.lua
@@ -1,3 +1,45 @@
+local function config()
+    local status_ok, dressing = pcall(require, "dressing")
+    if not status_ok then
+        return
+    end
+
+    dressing.setup({
+
+        input = {
+            enabled = false,
+            relative = "cursor",
+            -- relative = "editor",
+            prefer_width = 100,
+            width = nil,
+            -- min_width and max_width can be a list of mixed types.
+            -- min_width = {20, 0.2} means "the greater of 20 columns or 20% of total"
+            max_width = { 140, 0.9 },
+            min_width = { 20, 0.2 },
+            win_options = {
+                winblend = 0,
+            },
+            get_config = function()
+                if vim.api.nvim_buf_get_option(0, "filetype") == "NvimTree" then
+                    return { enabled = false }
+                end
+            end,
+        },
+
+        select = {
+            -- backend = { "telescope", "nui", "builtin" },
+            border = "rounded",
+            get_config = function(opts)
+                if opts.kind == "codeaction" then
+                    return {
+                        telescope = require("telescope.themes").get_cursor(),
+                    }
+                end
+            end,
+        }
+
+    })
+end
 return {
-    { 'stevearc/dressing.nvim', enabled = true, lazy = false, },
+    { 'stevearc/dressing.nvim', enabled = true, lazy = false, config = config },
 }
