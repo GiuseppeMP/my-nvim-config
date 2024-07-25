5713b9ef64b7e5aa5937417ec7ed3ece80f8d461
Peppe <giuseppemath@gmail.com>
2024-07-25 18:51:12 -0300

    (fix): telescope bug when using plenary.

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -1,6 +1,6 @@
 vim.g.lazygit_floating_window_winblend = 0       -- transparency of floating window
 vim.g.lazygit_floating_window_scaling_factor = 1 -- scaling factor for floating window
-vim.g.lazygit_floating_window_use_plenary = 1    -- use plenary.nvim to manage floating window if available
+vim.g.lazygit_floating_window_use_plenary = 0    -- use plenary.nvim to manage floating window if available
 vim.g.lazygit_use_neovim_remote = 1              -- fallback to 0 if neovim-remote is not installed
 vim.g.lazygit_use_custom_config_file_path = 0    -- config file path is evaluated if this value is 1
 
