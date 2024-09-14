591b88933dbac304d446b111066ac3cbd6a8c16c
GiuseppeMP <giuseppemath@gmail.com>
2024-09-14 15:04:16 -0300

    (feat): add dap-python bindings.

--- lua/plugins/lsp/pyright.lua
+++ lua/plugins/lsp/pyright.lua
@@ -2,6 +2,12 @@ local on_attach_pyright = function(client, bufnr)
     -- default on_attach
     require("plugins.lsp.utils.on_attach_options").get { format_on_save = true, format = true, lsp_client =
     'pyright' } (client, bufnr)
+
+    vim.cmd [[
+        nnoremap <silent> dn :lua require('dap-python').test_method()<CR>
+        nnoremap <silent> df :lua require('dap-python').test_class()<CR>
+        vnoremap <silent> ds <ESC>:lua require('dap-python').debug_selection()<CR>
+    ]]
 end
 
 require('lspconfig').pyright.setup {
