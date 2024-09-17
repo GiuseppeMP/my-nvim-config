96ad5b5708c53bb5f626758d5f12570957ced1e4
GiuseppeMP <giuseppemath@gmail.com>
2024-09-16 23:02:29 -0300

    (fix): vim-table-mode disabled, it's conflicting keybinds.

--- lua/plugins/vim-table.lua
+++ lua/plugins/vim-table.lua
@@ -1,6 +1,7 @@
 return { {
     'dhruvasagar/vim-table-mode',
     ft = { 'markdown', 'text', 'text', 'plaintext', 'norg' },
+    enabled = false,
     config = function()
         vim.g.table_mode_disable_mappings = 1
     end
