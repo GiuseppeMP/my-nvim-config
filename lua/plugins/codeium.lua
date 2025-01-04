7cc78284204642ba3562e9dcf4ad46f368647354
GiuseppeMP <giuseppemath@gmail.com>
2025-01-04 16:10:04 +0100

    (fix): codeium github url.

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,37 +1,29 @@
 return {
-    {
-        "Exafunction/codeium.vim",
-        event = 'BufEnter',
+    "Exafunction/codeium.nvim",
+    event = 'VeryLazy',
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "hrsh7th/nvim-cmp",
     },
-    {
-        "jcdickinson/codeium.nvim",
-        enabled = true,
-        event = 'VeryLazy',
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-        },
-        cond = function() return conf.user.codeium.enabled end,
-        config = function()
-            vim.g.codeium_manual = true
-            vim.g.codeium_disable_bindings = 1
-            ---@diagnostic disable-next-line: different-requires
-            require("codeium").setup({
-                enable_chat = true,
-                workspace_root = {
-                    use_lsp = true,
-                    find_root = nil,
-                    paths = {
-                        ".bzr",
-                        ".git",
-                        ".hg",
-                        ".svn",
-                        "_FOSSIL_",
-                        "package.json",
-                        "pom.xml",
-                    }
+    config = function()
+        vim.g.codeium_manual = true
+        vim.g.codeium_disable_bindings = 1
+        require("codeium").setup({
+            map_keys = true,
+            enable_chat = true,
+            workspace_root = {
+                use_lsp = false,
+                find_root = nil,
+                paths = {
+                    ".bzr",
+                    ".git",
+                    ".hg",
+                    ".svn",
+                    "_FOSSIL_",
+                    "package.json",
+                    "pom.xml",
                 }
-            })
-        end
-    },
+            }
+        })
+    end
 }
