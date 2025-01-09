92f5abe15d39d311afbc189d3b36ac1ad15400ec
GiuseppeMP <giuseppemath@gmail.com>
2025-01-09 08:59:51 +0100

    (fix): dap panel with wrong render.

--- lua/plugins/dap/dap_setup.lua
+++ lua/plugins/dap/dap_setup.lua
@@ -77,7 +77,6 @@ require("dapui").setup({
     force_buffers = true,
     icons = { expanded = "", collapsed = "", current_frame = "" },
     mappings = {
-        -- Use a table to apply multiple mappings
         expand = { "<CR>", "<2-LeftMouse>" },
         open = "o",
         remove = "d",
@@ -87,22 +86,12 @@ require("dapui").setup({
     },
     -- Use this to override mappings for specific elements
     element_mappings = {
-        -- Example:
-        -- stacks = {
-        --   open = "<CR>",
-        --   expand = "o",
-        -- }
+        stacks = {
+            open = "<CR>",
+            expand = "o",
+        }
     },
-    -- Expand lines larger than the window
-    -- Requires >= 0.7
     expand_lines = vim.fn.has("nvim-0.7") == 1,
-    -- Layouts define sections of the screen to place windows.
-    -- The position can be "left", "right", "top" or "bottom".
-    -- The size specifies the height/width depending on position. It can be an Int
-    -- or a Float. Integer specifies height/width directly (i.e. 20 lines/columns) while
-    -- Float value specifies percentage (i.e. 0.3 - 30% of available lines/columns)
-    -- Elements are the elements shown in the layout (in order).
-    -- Layouts are opened in order so that earlier layouts take priority in window sizing.
     layouts = {
         {
             elements = {
@@ -112,12 +101,11 @@ require("dapui").setup({
                 "watches",
                 "stacks",
             },
-            size = 30, -- 40 columns
+            size = 40,
             position = "left",
         },
         {
             elements = {
-                "repl",
                 "console",
             },
             size = 0.25, -- 25% of total lines
@@ -148,11 +136,9 @@ require("dapui").setup({
             close = { "q", "<Esc>" },
         },
     },
-    windows = { indent = 1 },
     render = {
-        indent = 240,
-        max_type_length = nil, -- Can be integer or nil.
-        max_value_lines = 100, -- Can be integer or nil.
+        indent = 1,
+        max_value_lines = 100
     }
 })
 
