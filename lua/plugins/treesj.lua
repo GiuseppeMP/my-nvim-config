e80630cc36d37b4bf5175faf40e8039a0fe65ee1
GiuseppeMP <giuseppemath@gmail.com>
2025-02-14 18:21:43 +0100

    (map): treesj remap to `<leader>mm`

--- lua/plugins/treesj.lua
+++ lua/plugins/treesj.lua
@@ -2,7 +2,7 @@ return {
     'Wansmer/treesj',
     dependencies = { 'nvim-treesitter/nvim-treesitter' }, -- if you install parsers with `nvim-treesitter`
     config = function()
-        vim.keymap.set('n', '<leader>m', require('treesj').toggle)
+        vim.keymap.set('n', '<leader>mm', require('treesj').toggle)
         vim.keymap.set('n', '<leader>M', function() require('treesj').toggle({ split = { recursive = true } }) end)
     end,
 }
