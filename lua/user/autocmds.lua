085591de3d77b9b9f204ec2b82bab42d894572ea
GiuseppeMP <giuseppemath@gmail.com>
2025-03-14 13:22:04 +0100

    (feat): dd qf items

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -63,3 +63,19 @@ vim.api.nvim_create_autocmd("VimEnter", {
         ]])
     end
 })
+vim.cmd [[
+
+" When using `dd` in the quickfix list, remove the item from the quickfix list.
+function! RemoveQFItem()
+  let curqfidx = line('.') - 1
+  let qfall = getqflist()
+  call remove(qfall, curqfidx)
+  call setqflist(qfall, 'r')
+  execute curqfidx + 1 . "cfirst"
+  :copen
+endfunction
+:command! RemoveQFItem :call RemoveQFItem()
+" Use map <buffer> to only map dd in the quickfix window. Requires +localmap
+autocmd FileType qf map <buffer> dd :RemoveQFItem<cr>
+
+]]
