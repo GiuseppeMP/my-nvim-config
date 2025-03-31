d1e0468475294ad7522e1d126de3cc987ce17d74
GiuseppeMP <giuseppemath@gmail.com>
2025-03-31 09:29:39 +0200

    (fix): gitsigns icon size decreased

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,10 +1,14 @@
 local function config()
     require('gitsigns').setup {
         signs                        = {
-            add          = { text = '▌' },
-            change       = { text = '▌' },
-            delete       = { text = '▬' },
-            topdelete    = { text = '▀' },
+            -- add          = { text = '▌' },
+            -- change       = { text = '▌' },
+            -- delete       = { text = '▬' },
+            -- topdelete    = { text = '▀' },
+            add          = { text = '▎' },
+            change       = { text = '▎' },
+            delete       = { text = '▎' },
+            topdelete    = { text = '▎' },
             changedelete = { text = '~' },
             untracked    = { text = '┆' },
         },
