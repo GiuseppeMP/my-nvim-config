60a4affc38774216b4de61c238c6fd9af1ee90ce
GiuseppeMP <giuseppemath@gmail.com>
2025-04-18 18:21:35 +0200

    (fix): windline 0.10.4

--- lua/plugins/windline/init.lua
+++ lua/plugins/windline/init.lua
@@ -1,7 +1,11 @@
 return
 {
     'windwp/windline.nvim',
+    enabled = true,
     config = function()
+        -- require('wlsample.bubble')
+        -- require('wlsample.bubble2')
+        -- require('wlsample.evil_line')
         -- require 'lua.plugins.windline.evil_line'
         require 'plugins.windline.beppe_line'
     end
