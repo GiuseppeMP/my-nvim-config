8044786ccbd517830ca442311891d59f80ca57d0
GiuseppeMP <giuseppemath@gmail.com>
2025-04-18 18:22:58 +0200

    (feat): add initial draft of integration Wezterm

--- lua/user/autocmds.lua
+++ lua/user/autocmds.lua
@@ -84,3 +84,14 @@ autocmd FileType qf map <buffer> dd :RemoveQFItem<cr>
 vim.cmd [[
     autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o
 ]]
+
+-- vim.api.nvim_create_autocmd({ "BufEnter" }, {
+--     callback = function(event)
+--         local title = "vim"
+--         if event.file ~= "" then
+--             title = string.format("vim: %s", vim.fs.basename(event.file))
+--         end
+--
+--         vim.fn.system({ "wezterm", "cli", "set-tab-title", title })
+--     end,
+-- })
