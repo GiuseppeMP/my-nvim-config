c0f938ff54cf6d82650b03e5ace34ff1bcf61910
Giuseppe Matheus <giuseppemath@gmail.com>
2025-05-17 21:45:50 +0200

    (vim-test) remove old configs

--- lua/plugins/vim-test.lua
+++ lua/plugins/vim-test.lua
@@ -2,8 +2,10 @@ local function config()
     local g = vim.g
     g.ultest_use_pty = 1
     vim.cmd [[
-        let  g:test#java#maventest#test_command = "-DunitOnly=true"
+        let g:test#java#maventest#test_command = "-DunitOnly=true"
         let test#custom_runners = {'Groovy': ['Maventest']}
+        let g:test#js#jest#test_command = '--runInBand'
+        let g:test#javascript#jest#test_command = '--runInBand'
     ]]
 
     g['test#strategy'] = {
@@ -12,10 +14,6 @@ local function config()
         nearest = 'floaterm',
         file = 'floaterm',
     }
-
-    vim.cmd [[ command! -nargs=* -bar DebugTestNearest call test#run('debug_nearest', split(<q-args>)) ]]
-
-    vim.cmd [[ command! -nargs=* -bar DebugTestFile call test#run('debug_file', split(<q-args>)) ]]
 end
 return {
     { 'vim-test/vim-test', config = config },
