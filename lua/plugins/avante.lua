c9d8bf405c7c829e145bbd8c3cc37064a225bdbe
GiuseppeMP <giuseppemath@gmail.com>
2025-05-27 19:15:04 +0200

    (avante): improve configs

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -8,11 +8,47 @@ return {
         provider = "openai",
         openai = {
             endpoint = "https://api.openai.com/v1",
-            model = "gpt-4o",       -- your desired model (or use gpt-4o, etc.)
-            timeout = 30000,        -- Timeout in milliseconds, increase this for reasoning models
+            model = "gpt-4o",             -- your desired model (or use gpt-4o, etc.)
+            timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
             temperature = 0,
             max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
             --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        },
+        hints = { enabled = true },
+        behaviour = {
+            auto_suggestions = false, -- Experimental stage
+            auto_set_highlight_group = true,
+            auto_set_keymaps = true,
+            auto_apply_diff_after_generation = false,
+            support_paste_from_clipboard = true,
+            minimize_diff = true,         -- Whether to remove unchanged lines when applying a code block
+            enable_token_counting = true, -- Whether to enable token counting. Default to true.
+        },
+        windows = {
+            ---@type "right" | "left" | "top" | "bottom"
+            position = "right",   -- the position of the sidebar
+            wrap = true,          -- similar to vim.o.wrap
+            width = 30,           -- default % based on available width
+            sidebar_header = {
+                enabled = true,   -- true, false to enable/disable the header
+                align = "center", -- left, center, right for title
+                rounded = true,
+            },
+            input = {
+                prefix = "> ",
+                height = 8, -- Height of the input window in vertical layout
+            },
+            edit = {
+                border = "rounded",
+                start_insert = true, -- Start insert mode when opening the edit window
+            },
+            ask = {
+                floating = false,    -- Open the 'AvanteAsk' prompt in a floating window
+                start_insert = true, -- Start insert mode when opening the ask window
+                border = "rounded",
+                ---@type "ours" | "theirs"
+                focus_on_apply = "ours", -- which diff to focus after applying
+            },
         },
     },
     -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
@@ -24,12 +60,12 @@ return {
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         --- The below dependencies are optional,
-        "echasnovski/mini.pick",     -- for file_selector provider mini.pick
+        "echasnovski/mini.pick",         -- for file_selector provider mini.pick
         "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
-        "hrsh7th/nvim-cmp",          -- autocompletion for avante commands and mentions
-        "ibhagwan/fzf-lua",          -- for file_selector provider fzf
-        "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-        "zbirenbaum/copilot.lua",    -- for providers='copilot'
+        "hrsh7th/nvim-cmp",              -- autocompletion for avante commands and mentions
+        "ibhagwan/fzf-lua",              -- for file_selector provider fzf
+        "nvim-tree/nvim-web-devicons",   -- or echasnovski/mini.icons
+        "zbirenbaum/copilot.lua",        -- for providers='copilot'
         {
             -- support for image pasting
             "HakonHarnes/img-clip.nvim",
