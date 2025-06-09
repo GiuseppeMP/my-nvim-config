8655a1e24292d5dd4059e8f5d1ab925780d2f045
GiuseppeMP <giuseppemath@gmail.com>
2025-06-09 13:25:06 +0200

    (bqf): add better quick fix plugin

---
+++ lua/plugins/bqf.lua
@@ -0,0 +1,9 @@
+return {
+    {
+        "kevinhwang91/nvim-bqf",
+        opts = {},
+        -- config = function()
+        --     require("bqf").setup({ auto_enable = true })
+        -- end
+    }
+}
