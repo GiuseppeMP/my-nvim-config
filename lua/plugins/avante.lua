2fec7cc1c31fae20ef4f7e44c53c8decd027c640
GiuseppeMP <giuseppemath@gmail.com>
2025-06-09 13:35:58 +0200

    (avante): update breakchanges config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -6,13 +6,16 @@ return {
         -- add any opts here
         -- for example
         provider = "openai",
-        openai = {
-            endpoint = "https://api.openai.com/v1",
-            model = "gpt-4o",             -- your desired model (or use gpt-4o, etc.)
-            timeout = 30000,              -- Timeout in milliseconds, increase this for reasoning models
-            temperature = 0,
-            max_completion_tokens = 8192, -- Increase this to include reasoning tokens (for reasoning models)
-            --reasoning_effort = "medium", -- low|medium|high, only used for reasoning models
+        providers = {
+            openai = {
+                endpoint = "https://api.openai.com/v1",
+                model = "gpt-4o",    -- your desired model (or use gpt-4o, etc.)
+                extra_request_body = {
+                    timeout = 30000, -- Timeout in milliseconds, increase this for reasoning models
+                    temperature = 0,
+                    max_completion_tokens = 8192
+                }
+            }
         },
         hints = { enabled = true },
         behaviour = {
