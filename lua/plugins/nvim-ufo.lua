1d033ff4da775ebc82fbc610892c884244cfdae6
GiuseppeMP <giuseppemath@gmail.com>
2025-06-09 13:25:19 +0200

    (nvim-ufo): remove lazy config

--- lua/plugins/nvim-ufo.lua
+++ lua/plugins/nvim-ufo.lua
@@ -3,10 +3,9 @@ return
 {
     "kevinhwang91/nvim-ufo",
     dependencies = { 'kevinhwang91/promise-async' },
-    event = 'VeryLazy',
     config = function()
-        vim.o.foldcolumn = '1'     -- '0' is not bad
-        vim.o.foldlevel = 99       -- Using ufo provider need a large value, feel free to decrease the value
+        vim.o.foldcolumn = '1' -- '0' is not bad
+        vim.o.foldlevel = 99   -- Using ufo provider need a large value, feel free to decrease the value
         vim.o.foldlevelstart = 99
         vim.o.foldenable = true
         require 'ufo'.setup({
