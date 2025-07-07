6c0ad4bb06ad7e8256f827d68673fbaafbc13ec8
GiuseppeMP <giuseppemath@gmail.com>
2025-07-07 22:18:32 +0200

    (blink): draw kind icons

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,3 +1,71 @@
+local drawWithMiniIcons = function()
+    return {
+        draw = {
+            components = {
+                kind_icon = {
+                    text = function(ctx)
+                        local kind_icon, _, _ = require('mini.icons').get('lsp', ctx.kind)
+                        return kind_icon
+                    end,
+                    -- (optional) use highlights from mini.icons
+                    highlight = function(ctx)
+                        local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                        return hl
+                    end,
+                },
+                kind = {
+                    -- (optional) use highlights from mini.icons
+                    highlight = function(ctx)
+                        local _, hl, _ = require('mini.icons').get('lsp', ctx.kind)
+                        return hl
+                    end,
+                }
+            }
+        },
+        border = 'single'
+    }
+end
+
+local drawWithDevIcons = function()
+    return
+    {
+        components = {
+            kind_icon = {
+                text = function(ctx)
+                    local icon = ctx.kind_icon
+                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                        local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                        if dev_icon then
+                            icon = dev_icon
+                        end
+                    else
+                        icon = require("lspkind").symbolic(ctx.kind, {
+                            mode = "symbol",
+                        })
+                    end
+
+                    return icon .. ctx.icon_gap
+                end,
+
+                -- Optionally, use the highlight groups from nvim-web-devicons
+                -- You can also add the same function for `kind.highlight` if you want to
+                -- keep the highlight groups in sync with the icons.
+                highlight = function(ctx)
+                    local hl = ctx.kind_hl
+                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                        local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                        if dev_icon then
+                            hl = dev_hl
+                        end
+                    end
+                    return hl
+                end,
+            }
+        },
+        border = 'single'
+    }
+end
+
 return {
     'saghen/blink.cmp',
     dependencies = { 'rafamadriz/friendly-snippets', 'L3MON4D3/LuaSnip' },
@@ -50,18 +118,22 @@ return {
 
                 -- (Default) Only show the documentation popup when manually triggered
                 completion = {
-                    trigger = {
+                    trigger       = {
                         show_in_snippet = true
                     },
-                    list = {
+                    list          = {
                         selection = {
                             preselect = function(ctx) return not require('blink.cmp').snippet_active({ direction = 1 }) end,
                         }
                     },
-                    menu = { border = 'single' },
-                    documentation = { window = { border = 'single', style = 'rounded' } },
+                    menu          = drawWithDevIcons(),
+                    -- Show documentation when selecting a completion item
+                    documentation = { auto_show = true, auto_show_delay_ms = 500, window = { border = 'single' } },
+
+                    -- Display a preview of the selected item on the current line
+                    ghost_text    = { enabled = true },
                 },
-                signature = { window = { border = 'single', style = 'rounded' } },
+                signature = { window = { border = 'single' } },
 
                 -- Default list of enabled providers defined so that you can extend it
                 -- elsewhere in your config, without redefining it, due to `opts_extend`
@@ -72,7 +144,18 @@ return {
                 sources = {
                     default = { 'lsp', 'path', 'snippets', 'buffer', 'codeium' },
                     providers = {
-                        codeium = { name = 'Codeium', module = 'codeium.blink', async = true },
+                        codeium = {
+                            name = 'Codeium',
+                            module = 'codeium.blink',
+                            async = true,
+                            transform_items = function(ctx, items)
+                                for _, item in ipairs(items) do
+                                    item.kind_icon = ''
+                                    item.kind_name = 'Codeium'
+                                end
+                                return items
+                            end
+                        },
                     },
                 },
 
@@ -81,7 +164,11 @@ return {
                 -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
                 --
                 -- See the fuzzy documentation for more information
-                fuzzy = { implementation = "prefer_rust_with_warning" }
+                fuzzy = { implementation = "prefer_rust_with_warning" },
+                cmdline = {
+                    keymap = { preset = 'inherit' },
+                    completion = { menu = { auto_show = true } },
+                },
             }
         )
     end
