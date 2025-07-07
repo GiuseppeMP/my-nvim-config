f8c88ac7ceb62396dc9e7e7aec98c418b50e61a2
GiuseppeMP <giuseppemath@gmail.com>
2025-07-07 21:58:09 +0200

    (markdown-preview): fix build

--- lua/plugins/markdown-preview.lua
+++ lua/plugins/markdown-preview.lua
@@ -4,5 +4,9 @@ return
     event = { "VeryLazy" },
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
     ft = { "markdown" },
-    build = function() vim.fn["mkdp#util#install"]() end,
+    build = function()
+        vim.cmd [[Lazy load markdown-preview.nvim]]
+        vim.fn['mkdp#util#install']()
+    end,
+    -- build = function() vim.fn["mkdp#util#install"]() end,
 }
