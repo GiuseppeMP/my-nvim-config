77e9a3f50d586b00bd8265a0d8d0ab8fddae96ac
GiuseppeMP <giuseppemath@gmail.com>
2025-07-07 22:27:27 +0200

    (blink): add lsp caps to blink

--- lua/plugins/lsp/utils/capabilities_options.lua
+++ lua/plugins/lsp/utils/capabilities_options.lua
@@ -1,11 +1,11 @@
 M = {}
 
 M.get = function(params)
-    local caps = vim.lsp.protocol.make_client_capabilities()
+    -- local caps = vim.lsp.protocol.make_client_capabilities()
+    local caps = require('blink.cmp').get_lsp_capabilities()
     if caps.workspace then
         caps.workspace.didChangeWatchedFiles = nil
     end
-    caps.textDocument.completion.completionItem.snippetSupport = true
     if (params.cmp_nvim == nil or params.cmp_nvim) then
         -- caps = require('cmp_nvim_lsp').default_capabilities(caps)
         -- if caps.workspace then
@@ -13,8 +13,10 @@ M.get = function(params)
         -- end
         -- return caps
     end
+    caps.textDocument.completion.completionItem.snippetSupport = true
+    return caps
 end
 
-M.default = M.get({ cmp_nvim = true })
+M.default = M.get({ cmp_nvim = false, blink = true })
 
 return M
