027b0e49e08f995ecaa4366a42b1981a5289203a
GiuseppeMP <giuseppemath@gmail.com>
2025-10-17 19:10:17 +0200

    (marksman): migrate to vim.lsp.config

--- lua/plugins/lsp/marksman.lua
+++ lua/plugins/lsp/marksman.lua
@@ -1,8 +1,7 @@
 local on_attach_options = require("plugins.lsp.utils.on_attach_options")
 local capabilities_options = require("plugins.lsp.utils.capabilities_options")
-local lspconfig = require 'lspconfig'
 
-lspconfig.marksman.setup {
+vim.lsp.config('marksman', {
     on_attach = on_attach_options.get { lsp_client = 'marksman' },
     capabilities = capabilities_options.default
-}
+})
