b00eb3f3bd2e2ac39afd78b19da3799f0838a5b4
GiuseppeMP <giuseppemath@gmail.com>
2025-10-17 19:10:56 +0200

    (jsonls): migrate to vim.lsp.config

--- lua/plugins/lsp/jsonls.lua
+++ lua/plugins/lsp/jsonls.lua
@@ -1,8 +1,7 @@
 local on_attach_options = require("plugins.lsp.utils.on_attach_options")
 local capabilities_options = require("plugins.lsp.utils.capabilities_options")
-local lspconfig = require 'lspconfig'
 
-lspconfig.jsonls.setup {
+vim.lsp.config('jsonls', {
     on_attach = on_attach_options.get { lsp_client = 'jsonls' },
     capabilities = capabilities_options.default
-}
+})
