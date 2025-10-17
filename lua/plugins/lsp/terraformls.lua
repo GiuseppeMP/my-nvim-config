03a43eaaaefe9981fea76734773574abec4a6cef
GiuseppeMP <giuseppemath@gmail.com>
2025-10-17 19:09:29 +0200

    (terraformls): migrate to vim.lsp.config

--- lua/plugins/lsp/terraformls.lua
+++ lua/plugins/lsp/terraformls.lua
@@ -1,9 +1,8 @@
-local lspconfig = require 'lspconfig'
 local on_attach_options = require("plugins.lsp.utils.on_attach_options")
 local capabilities_options = require("plugins.lsp.utils.capabilities_options")
 
 
-lspconfig.terraformls.setup {
+vim.lsp.config('terraformls', {
     on_attach = on_attach_options.get { lsp_client = 'terraformls' },
     capabilities = capabilities_options.default
-}
+})
