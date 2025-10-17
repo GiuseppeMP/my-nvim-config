9b63054b661f6fc950ab7abeb91f73e7d1388d42
GiuseppeMP <giuseppemath@gmail.com>
2025-10-17 19:11:28 +0200

    (gopls): migrate to vim.lsp.config

--- lua/plugins/lsp/gopls.lua
+++ lua/plugins/lsp/gopls.lua
@@ -1,9 +1,7 @@
-local lspconfig = require 'lspconfig'
 local on_attach_options = require("plugins.lsp.utils.on_attach_options")
 local capabilities_options = require("plugins.lsp.utils.capabilities_options")
 
-
-lspconfig.gopls.setup {
+vim.lsp.config('gopls', {
     on_attach = on_attach_options.get { lsp_client = 'gopls' },
     capabilities = capabilities_options.default
-}
+})
