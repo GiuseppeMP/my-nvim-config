7bc7ca7c49a2607b4511312d0353eff96ad3d638
GiuseppeMP <giuseppemath@gmail.com>
2025-10-17 19:11:07 +0200

    (html): migrate to vim.lsp.config

--- lua/plugins/lsp/html.lua
+++ lua/plugins/lsp/html.lua
@@ -1,8 +1,7 @@
 local on_attach_options = require("plugins.lsp.utils.on_attach_options")
 local capabilities_options = require("plugins.lsp.utils.capabilities_options")
-local lspconfig = require 'lspconfig'
 
-lspconfig.html.setup {
+vim.lsp.config('html', {
     on_attach = on_attach_options.get { lsp_client = 'html' },
     capabilities = capabilities_options.default
-}
+})
