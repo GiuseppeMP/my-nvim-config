d3a30c7ccc76265fe199ab1ccf3978f8fcecc949
GiuseppeMP <giuseppemath@gmail.com>
2025-10-17 19:10:22 +0200

    (lua_ls): migrate to vim.lsp.config

--- lua/plugins/lsp/lua_ls.lua
+++ lua/plugins/lsp/lua_ls.lua
@@ -1,32 +1,51 @@
 local root_pattern = require 'lspconfig.util'.root_pattern
 
-require("lspconfig").lua_ls.setup {
-
+vim.lsp.config('lua_ls', {
     on_attach = require("plugins.lsp.utils.on_attach_options").get({ lsp_client = 'lua_ls' }),
+    on_init = function(client)
+        if client.workspace_folders then
+            local path = client.workspace_folders[1].name
+            if path ~= vim.fn.stdpath('config') and (vim.uv.fs_stat(path .. '/.luarc.json') or vim.uv.fs_stat(path .. '/.luarc.jsonc'))
+            then
+                return
+            end
+        end
 
-    root_dir = root_pattern(".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml",
-        "selene.yml", ".git"),
-
+        client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+                version = 'LuaJIT',
+                path = {
+                    'lua/?.lua',
+                    'lua/?/init.lua',
+                },
+            },
+            workspace = {
+                checkThirdParty = false,
+                library = {
+                    vim.env.VIMRUNTIME
+                }
+            }
+        })
+    end,
+    root_markers = { ".luarc.json", ".luarc.jsonc", ".luacheckrc", ".stylua.toml", "stylua.toml", "selene.toml", "selene.yml", ".git" },
     settings = {
         Lua = {
             runtime = {
-                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
                 version = 'LuaJIT',
             },
             diagnostics = {
-                -- Get the language server to recognize the `vim` global
                 globals = { 'vim' },
             },
             workspace = {
                 checkThirdParty = false,
-                -- Make the server aware of Neovim runtime files
                 library = vim.api.nvim_get_runtime_file("", true),
             },
             -- Do not send telemetry data containing a randomized but unique identifier
             telemetry = {
-                enable = false,
+                enable = true,
             },
             hint = { enable = true }
         }
     }
-}
+})
+vim.lsp.enable("lua_ls")
