16d85800eb1e80c0a2c0445e58c29e7320267f04
GiuseppeMP <giuseppemath@gmail.com>
2025-10-17 19:10:52 +0200

    (lemminx): migrate to vim.lsp.config

--- lua/plugins/lsp/lemminx.lua
+++ lua/plugins/lsp/lemminx.lua
@@ -1,8 +1,7 @@
 local on_attach_options = require("plugins.lsp.utils.on_attach_options")
 local capabilities_options = require("plugins.lsp.utils.capabilities_options")
-local lspconfig = require 'lspconfig'
 
-lspconfig.lemminx.setup {
+vim.lsp.config('lemminx', {
     on_attach = on_attach_options.get { lsp_client = 'lemminx' },
     capabilities = capabilities_options.default,
-}
+})
