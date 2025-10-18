7881447de978bbc01f914c4a170f4c732fc0d6f3
GiuseppeMP <giuseppemath@gmail.com>
2025-10-18 18:17:34 +0200

    (vim_projectionist): fix alternate to files in the same folder. (#9)

--- lua/plugins/vim_projectionist/python.lua
+++ lua/plugins/vim_projectionist/python.lua
@@ -5,11 +5,13 @@ return {
         ['test/*_test.py'] = { type = "test", alternate = { "src/{dirname}/{basename}.py" } },
         ['tests/**/test_*.py'] = { type = "test", alternate = { "src/{dirname}/{basename}.py" } },
         ['tests/*_test.py'] = { type = "test", alternate = { "src/{dirname}/{basename}.py" } },
-        ['src/*_test.py'] = { type = "test", alternate = { "src/{dirname}/{basename}.py" } },
-        ['src/test_*.py'] = { type = "test", alternate = { "src/{dirname}/{basename}.py" } },
+        ['src/*_test.py'] = { type = "type", alternate = { "src/{}.py" } },
+        ['src/**/test_*.py'] = { type = "type", alternate = { "src/{}.py" } },
         ['src/*.py'] = {
             type = "source",
             alternate = {
+                "src/{}_test.py",
+                "src/test_{}.py",
                 "src/{dirname}/{basename}_test.py",
                 "src/{dirname}/test_{basename}.py",
                 "test/{dirname}/{basename}_test.py",
