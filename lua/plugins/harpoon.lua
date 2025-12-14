af59c066e92291e8e1393710b6c0a9d8483a2616
GiuseppeMP <giuseppemath@gmail.com>
2025-12-14 21:37:45 +0100

    (harpoon): remove telescope and fix usability. (#34)

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,85 +1,19 @@
-local telescope_state = require("telescope.actions.state")
-local telescope_finders = require("telescope.finders")
-local telescope_pickers = require("telescope.pickers")
-local harpoon_get_paths = function(files)
-    local paths = {}
-    local items = files.items
-    local len = files._length
-
-    for i = 1, len do
-        paths[i] = ''
-        local item = items[i]
-        if item ~= nil then
-            paths[i] = item.value
-        end
-    end
-
-    return paths
-end
-
-local function harpoon_make_finder(paths)
-    return telescope_finders.new_table({ results = paths })
-end
-
-local function config()
-    local harpoon = require 'harpoon'
+local function conf()
+    local harpoon = require("harpoon")
     harpoon:setup()
-    ---@type HarpoonList
-    local term_list = harpoon:list("terms") -- note the : instead of .
+    vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
+    vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-    ---@return string name of the created terminal
-    local function create_terminal()
-        vim.cmd("terminal")
-        local buf_id = vim.api.nvim_get_current_buf()
-        return vim.api.nvim_buf_get_name(buf_id)
-    end
+    vim.keymap.set("n", "<C-1>", function() harpoon:list():select(1) end)
+    vim.keymap.set("n", "<C-2>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-3>", function() harpoon:list():select(3) end)
+    vim.keymap.set("n", "<C-4>", function() harpoon:list():select(4) end)
 
-    ---@param index number: The index of the terminal to select.
-    local function select_term(index)
-        if index > term_list:length() then
-            create_terminal()
-            print("Creating terminal", index)
-            -- just append the newly open terminal
-            term_list:add() -- using add() as append() is depricated
-        else
-            -- find in list
-            print("selecting terminal", index)
-            term_list:select(index)
-        end
-    end
-
-    local conf = require("telescope.config").values
-    local function toggle_telescope(harpoon_files)
-        local file_paths = {}
-        for _, item in ipairs(harpoon_files.items) do
-            table.insert(file_paths, item.value)
-        end
-        require("telescope.pickers").new({}, {
-            prompt_title = "Harpoon",
-            finder = require("telescope.finders").new_table({
-                results = file_paths,
-            }),
-            previewer = conf.file_previewer({}),
-            sorter = conf.generic_sorter({}),
-            attach_mappings = function(prompt_buffer_number, map)
-                -- map("i", "<C-g>", function()
-                --     toggle_telescope(harpoon:list())
-                -- end)
-                -- map("i", "<C-x>", function()
-                --     local selected_entry = telescope_state.get_selected_entry()
-                --     local current_picker = telescope_state.get_current_picker(prompt_buffer_number)
-                --     harpoon:list():remove_at(selected_entry.index)
-                --     current_picker:refresh(harpoon_make_finder(harpoon_get_paths(harpoon:list())))
-                -- end)
-                return true
-            end
-        }):find()
-    end
-    -- basic telescope configuration
-
-    vim.keymap.set("n", "<leader>hh", function() toggle_telescope(harpoon:list()) end, { desc = "Open harpoon window" })
-    vim.keymap.set("n", "<leader>ht", function() select_term(1) end, { desc = "Select harpoon term 1" })
+    -- Toggle previous & next buffers stored within Harpoon list
+    vim.keymap.set("n", "<C-S-h>", function() harpoon:list():prev() end)
+    vim.keymap.set("n", "<C-S-l>", function() harpoon:list():next() end)
 end
+
 return {
-    { 'ThePrimeagen/harpoon', config = config, branch = 'harpoon2' },
+    { 'ThePrimeagen/harpoon', config = conf, branch = 'harpoon2' },
 }
